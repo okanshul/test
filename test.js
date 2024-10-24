a20bacdff2ae38778167bbda88deb9f0dbfe6314
Anshul <122551659+okanshul@users.noreply.github.com>
2024-10-24 12:18:23 +0530

    24-10-2024

---
+++ test.js
@@ -0,0 +1,37 @@
+const forms = document.getElementsByTagName('form')[0].addEventListener('submit', Form_Submit);
+
+function Form_Submit(event) {
+    event.preventDefault();
+    // Clear previous validation errors
+    const invalidFields = document.querySelectorAll('.is-invalid');
+    invalidFields.forEach(field => {
+        field.classList.remove('is-invalid');
+        field.nextElementSibling.innerHTML = ''; // Clear previous error messages
+    });
+    // Form submit for fech method
+    fetch(event.target.action, {
+        method: 'POST',
+        body: new FormData(event.target)
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not OK');
+    }).then(data => {
+        // errror message for invalid data
+        if (data.status == 400) {
+            // console.log(data.errors);
+            for (const [key, value] of Object.entries(data.errors)) {
+                document.getElementById(key).classList.add('is-invalid');
+                document.getElementById(key).nextElementSibling.classList.add('invalid-feedback', 'fs-0', 'ms-3');
+                document.getElementById(key).nextElementSibling.innerHTML = value[0];
+            }
+        }
+        // success message for valid data
+        if (data.status == 200) {
+            window.location.href = "{{ route('admin.dashboard') }}";
+        }
+    }).catch(error => {
+        alert(error);
+    });
+};
